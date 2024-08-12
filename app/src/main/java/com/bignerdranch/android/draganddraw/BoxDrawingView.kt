36515633a7349a3b1440662fa25d91070df2897d
vwhwang <vwhwang38@gmail.com>
2024-08-11 17:00:58 -0700

    first commit

---
+++ app/src/main/java/com/bignerdranch/android/draganddraw/BoxDrawingView.kt
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.draganddraw
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class BoxDrawingView(
+    context: Context,
+    attrs: AttributeSet? = null
+) : View(context, attrs) {
+}
