b354512c1d2a76689d34ad34b4e656b3daa8761c
vwhwang <vwhwang38@gmail.com>
2024-08-12 21:12:18 -0700

    Ch24 challenge 1 saving state on custom view

--- app/src/main/java/com/bignerdranch/android/draganddraw/BoxDrawingView.kt
+++ app/src/main/java/com/bignerdranch/android/draganddraw/BoxDrawingView.kt
@@ -4,25 +4,45 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.PointF
+import android.os.Bundle
+import android.os.Parcelable
 import android.util.AttributeSet
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 
 private const val TAG = "BoxDrawingView"
+private const val BOXES = "BOXES"
+private const val VIEW_STATE = "VIEW_STATE"
 class BoxDrawingView(
     context: Context,
     attrs: AttributeSet? = null
 ) : View(context, attrs) {
 
     private var currentBox: Box? = null
-    private val boxes = mutableListOf<Box>()
+    private var boxes = mutableListOf<Box>()
     private val boxPaint = Paint().apply {
         color = 0x22ff0000.toInt()
     }
     private val backgroundPaint = Paint().apply {
         color = 0xfff8efe0.toInt()
     }
+
+    override fun onSaveInstanceState(): Parcelable? {
+        val state = super.onSaveInstanceState()
+        val bundle = Bundle()
+        bundle.putParcelableArrayList(BOXES, ArrayList<Parcelable>(boxes))
+        bundle.putParcelable(VIEW_STATE, state)
+        return bundle
+    }
+
+    override fun onRestoreInstanceState(state: Parcelable?) {
+        if (state is Bundle){
+            boxes = state.getParcelableArrayList<Box>(BOXES)?.toMutableList() ?: mutableListOf()
+            super.onRestoreInstanceState(state.getParcelable(VIEW_STATE))
+        }
+    }
+
     override fun onTouchEvent(event: MotionEvent): Boolean {
         val current = PointF(event.x, event.y)
         var action = ""
